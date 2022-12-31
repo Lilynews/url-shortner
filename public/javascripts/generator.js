f970c843d8e38995d35920c8ddcf2d5252b51efb
Lilynews <lilinews2021@gmail.com>
2022-12-31 12:39:35 -0500

    feat: edit router to connect url to shorten

--- public/javascripts/generator.js
+++ public/javascripts/generator.js
@@ -1,25 +1,44 @@
+const randomLength = 5
+const urlDB = require('../../models/url')
+
+
 // define sample function to randomly return an item in an array
 function sample(array) {
   const index = Math.floor(Math.random() * array.length)
   return array[index]
 }
 
-// define generator function
-function generator(options) {
+// Generate randomNumber function
+function randomNumber(randomLength) {
   const lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz'
   const upperCaseLetters = lowerCaseLetters.toUpperCase()
   const numbers = '1234567890'
   let collection = numbers.concat(lowerCaseLetters).concat(upperCaseLetters)
-  const randomLength = 5
 
   // start generating randomNumber
   let randomNumber = ''
+
   for (let i = 0; i < randomLength; i++) {
     randomNumber += sample(collection)
   }
 
   return randomNumber
+
+}
+
+// non-repeated-shorten url
+function uniqueRandom() {
+  const uniqueRandomNumber = randomNumber(randomLength)
+  urlDB.findOne({ urlShortner: uniqueRandomNumber })
+    .lean()
+    .then(urlShortLink => {
+      if (urlShortLink) {
+        uniqueRandom()
+      }
+    })
+    .catch(error => console.log('uniqueRandom error'))
+  return uniqueRandomNumber
 }
 
 // export generatePassword function for other files to use
-module.exports = generator
+module.exports = uniqueRandom
